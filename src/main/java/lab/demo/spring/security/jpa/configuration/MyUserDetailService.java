d72ce5935f49adb4b3210f3a3f8a94889c2d77af
imHarish03 <harishhari302@gmail.com>
2019-11-30 23:25:34 +0530

    Simple spring security configured with Spring UserDetailService implementation

---
+++ src/main/java/lab/demo/spring/security/jpa/configuration/MyUserDetailService.java
@@ -0,0 +1,16 @@
+package lab.demo.spring.security.jpa.configuration;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return new MyUser(username);
+	}
+
+}
